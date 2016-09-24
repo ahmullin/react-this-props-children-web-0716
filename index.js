18a21cf381957012bbdd2eadc1b88636eac1f9db
Alison <alisonhmullin@gmail.com>
2016-09-24 17:11:27 -0400

    complete props children code along

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-const app = "I don't do much.";
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+export default class Panel extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-header">{this.props.title}</div>
+        <div className="panel-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default class MovieBrowser extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+
+    return (
+      <div className="movie-browser">
+        {childrenWithExtraProp}
+      </div>
+    );
+  }
+}
+
+export default class SomeComponent extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className="some-component-special-class">{child}</div>
+      );
+    });
+
+    return (
+      <div className="some-component">
+        <p>This component has {React.Children.count(this.props.children)} children.</p>
+        {childrenWithWrapperDiv}
+      </div>
+    );
+  }
+}
